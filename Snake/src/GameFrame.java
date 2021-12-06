21f88c88332eb541c3acc03576439133e60621a8
moadissa <95486027+moadissa@users.noreply.github.com>
2021-12-06 19:32:18 +0200

    Add files via upload

---
+++ Snake/src/GameFrame.java
@@ -0,0 +1,28 @@
+
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 74174
+ */
+public class GameFrame extends JFrame{
+
+    GameFrame(){
+     
+     this.add(new GamePanel());
+     this.setTitle("Snake");
+     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     this.setResizable(false);
+     this.pack();
+     this.setVisible(true);
+     this.setLocationRelativeTo(null);
+    
+}
+}
